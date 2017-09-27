e884336036d997af85ea3ece1193a02820549444
jroberts3 <jroberts2010@gmail.com>
2017-09-27 14:12:17 -0400

    Did switch case example

--- Week1-day3/Program.cs
+++ Week1-day3/Program.cs
@@ -73,6 +73,38 @@ namespace Week1_day3
 
 
             //}
+
+            Console.WriteLine("How many points did you earn?");
+            int pointsEarned = int.Parse(Console.ReadLine());
+
+            switch (pointsEarned)
+            {
+                case 0:
+                    Console.WriteLine("You lose.");
+                    break;
+                case 1:
+                    Console.WriteLine("Needs pratice.");
+                    break;
+                case 2:
+                    Console.WriteLine("On target.");
+                    break;
+                case 3:
+                    Console.WriteLine("Superstar.");
+                    break;
+                case 4:
+                    Console.WriteLine("Overachiever.");
+                    break;
+                case 5:
+                    Console.WriteLine("You cheated.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid score entered.");
+                    break;
+
+                  
+
+
+            }
         }
     }
 }
